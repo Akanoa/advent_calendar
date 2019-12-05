9255102ad710aeedf1878d0855e4e30bcc42434a
Yannick Guern <dev@guern.eu>
2019-12-05 16:36:46 +0100

    fix: compliant_to_strict_group_rule to pass tests

--- 2019/day04/src/lib.rs
+++ 2019/day04/src/lib.rs
@@ -58,58 +58,71 @@ fn compliant_to_double_rule(password: String) -> bool {
     false
 }
 
-fn compliant_to_no_more_than_double(password: String) -> bool {
-
-    println!("-------------");
-    println!("password: {}", password );
+fn compliant_to_strict_group_rule(password: String, number_of_element_by_group: i32) -> bool {
 
     let mut chars = password.chars().peekable();
-    let mut current = match chars.next() {
+    let mut checked_digit = match chars.next() {
         Some(c) => c,
         None => panic!("Empty password")
     };
-
-    println!("first digit {}", current);
-
-    let mut result = false;
-
+    let mut occurrence_digit_checked = 1;
 
     loop {
-        let next = match chars.next() {
-            Some(c) => c,
-            None => break
-        };
-
-        println!("next digit {}", next);
-
-        if next == current  {
-
-            let next_next = match chars.peek() {
-                Some(c) => *c,
-                None => return true
-            };
-
-            println!("next next digit {}", next_next);
-
-            if next != next_next  {
-                return true;
-            } else {
-                result = false;
+        // check for a next digit
+        match chars.next() {
+            // there is a next digit
+            Some(next) => {
+                // the next digit is the same that the current one but we cn't return true yet
+                // because this group can be more than a strict double group digit
+                if next == checked_digit {
+                    // because the next digit we can increase the counter
+                    occurrence_digit_checked += 1;
+                    // we check the next digit after the next
+                    match chars.peek() {
+                        // the next digit wasn't the last one
+                        Some(&next_next) => {
+                            // we set the current one to the next one for the next iteration
+                            checked_digit = next;
+                            // of the next after next is the same as current it's a larger group than 2
+                            // we continue to try to find some other strict digit group than the guessed
+                            if next_next == next {
+                                continue
+                            }
+                            // It's a strict double group of digit before the end of the string
+                            if occurrence_digit_checked == number_of_element_by_group {
+                                // Success :D
+                                return true
+                            }
+                        },
+                        // There is no more digit after the next one
+                        None => {
+                            // But the occurrence counter is 2
+                            if occurrence_digit_checked == number_of_element_by_group {
+                                // Success
+                                return true
+                            }
+                        }
+                    }
+                }
+                // the current and next digit are different,
+                // so we begin a new potential strict double digit group
+                // therefore we reset the occurrence counter
+                checked_digit = next;
+                occurrence_digit_checked = 1;
+            },
+            // There is no more digit after the current one
+            None => {
+                // It's a strict double group of digit before the end of the string
+                if occurrence_digit_checked == 2 {
+                    // Success
+                    return true
+                }
+                break
             }
-        }
-
-        current = match chars.next() {
-            Some(c) => c,
-            None => break
         };
 
     }
-
-    if !result {
-        println!("Password {} not compliant", password);
-    }
-
-    result
+    false
 }
 
 pub fn part1() -> u32 {
@@ -138,7 +151,7 @@ pub fn part2() -> u32 {
         if !compliant_to_double_rule(password_string.clone()) {
             continue;
         }
-        if !compliant_to_no_more_than_double(password_string.clone()) {
+        if !compliant_to_strict_group_rule(password_string.clone(), 2) {
             continue;
         }
         count+=1;
@@ -148,7 +161,7 @@ pub fn part2() -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{compliant_to_decreasing_rule, compliant_to_double_rule, compliant_to_no_more_than_double};
+    use crate::{compliant_to_decreasing_rule, compliant_to_double_rule, compliant_to_strict_group_rule};
 
     #[test]
     fn test_double_rule() {
@@ -167,14 +180,17 @@ mod tests {
 
     #[test]
     fn test_no_more_than_double() {
-
-//        assert_eq!(false, compliant_to_no_more_than_double("111111".to_string()), "not compliant to double rule 1");
-//        assert_eq!(true, compliant_to_no_more_than_double("113311".to_string()), "not compliant to double rule 1");
-        assert_eq!(compliant_to_no_more_than_double("11".to_string()), true, "not compliant to double rule 1");
-        assert_eq!(compliant_to_no_more_than_double("111".to_string()), false, "not compliant to double rule 1");
-//        assert_eq!(true, compliant_to_no_more_than_double("113334".to_string()), "not compliant to double rule 1");
-//        assert_eq!(true, compliant_to_no_more_than_double("112233".to_string()), "compliant to double rule");
-//        assert_eq!(true, compliant_to_no_more_than_double("111122".to_string()), "compliant to double rule");
-        assert_eq!(compliant_to_no_more_than_double("123444".to_string()), false, "not compliant to double rule 2");
+        assert_eq!(compliant_to_strict_group_rule("1".to_string(), 2), false, "not compliant to the only double rule only one '1' then end of string");
+        assert_eq!(compliant_to_strict_group_rule("11".to_string(), 2), true, "compliant to the only double rule at the end of string");
+        assert_eq!(compliant_to_strict_group_rule("111".to_string(), 2), false, "not compliant to the only double rule there is three '1' in row");
+        assert_eq!(compliant_to_strict_group_rule("1111".to_string(), 2), false, "not compliant to the only double rule there is four '1' in row");
+        assert_eq!(compliant_to_strict_group_rule("11111".to_string(), 2), false, "not compliant to the only double rule there is five '1' in row");
+        assert_eq!(compliant_to_strict_group_rule("111111".to_string(), 2), false, "not compliant to the only double rule there is six '1' in row");
+        assert_eq!(compliant_to_strict_group_rule("113311".to_string(), 2), true, "1 - compliant to rule there is a least one strict double");
+        assert_eq!(compliant_to_strict_group_rule("113334".to_string(), 2), true, "2 - compliant to rule there is a least one double, there two '1' at the beginning");
+        assert_eq!(compliant_to_strict_group_rule("123334".to_string(), 2), false, "not compliant to strict double rule, there isn't a strict double digit");
+        assert_eq!(compliant_to_strict_group_rule("112233".to_string(), 2), true, "compliant to the strict double rule, there is a double '2' in the middle of string");
+        assert_eq!(compliant_to_strict_group_rule("123444".to_string(), 2), false, "not compliant to strict double rule, there three '4' at the en ");
+        assert_eq!(compliant_to_strict_group_rule("111122".to_string(), 2), true, "compliant to double rule, there is a strict double '2' a the end of string");
     }
 }
