333d87edffa967abaf2849b3a0e31c445c133e63
Yannick Guern <dev@guern.eu>
2019-12-05 02:05:19 +0100

    buggy part 2

--- 2019/day04/src/lib.rs
+++ 2019/day04/src/lib.rs
@@ -27,7 +27,7 @@ fn compliant_to_decreasing_rule(password: String) -> bool{
 
         current = next;
 
-     }
+    }
 
     true
 }
@@ -58,6 +58,60 @@ fn compliant_to_double_rule(password: String) -> bool {
     false
 }
 
+fn compliant_to_no_more_than_double(password: String) -> bool {
+
+    println!("-------------");
+    println!("password: {}", password );
+
+    let mut chars = password.chars().peekable();
+    let mut current = match chars.next() {
+        Some(c) => c,
+        None => panic!("Empty password")
+    };
+
+    println!("first digit {}", current);
+
+    let mut result = false;
+
+
+    loop {
+        let next = match chars.next() {
+            Some(c) => c,
+            None => break
+        };
+
+        println!("next digit {}", next);
+
+        if next == current  {
+
+            let next_next = match chars.peek() {
+                Some(c) => *c,
+                None => return true
+            };
+
+            println!("next next digit {}", next_next);
+
+            if next != next_next  {
+                return true;
+            } else {
+                result = false;
+            }
+        }
+
+        current = match chars.next() {
+            Some(c) => c,
+            None => break
+        };
+
+    }
+
+    if !result {
+        println!("Password {} not compliant", password);
+    }
+
+    result
+}
+
 pub fn part1() -> u32 {
     let mut count : u32 = 0;
     for password in 264793..803935 {
@@ -74,9 +128,27 @@ pub fn part1() -> u32 {
     count
 }
 
+pub fn part2() -> u32 {
+    let mut count : u32 = 0;
+    for password in 264793..803935 {
+        let password_string = password.to_string();
+        if !compliant_to_decreasing_rule(password_string.clone()) {
+            continue;
+        }
+        if !compliant_to_double_rule(password_string.clone()) {
+            continue;
+        }
+        if !compliant_to_no_more_than_double(password_string.clone()) {
+            continue;
+        }
+        count+=1;
+    }
+    count
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{compliant_to_decreasing_rule, compliant_to_double_rule};
+    use crate::{compliant_to_decreasing_rule, compliant_to_double_rule, compliant_to_no_more_than_double};
 
     #[test]
     fn test_double_rule() {
@@ -91,5 +163,18 @@ mod tests {
         assert_eq!(true, compliant_to_decreasing_rule("111123".to_string()));
         assert_eq!(true, compliant_to_decreasing_rule("135679".to_string()));
         assert_eq!(false, compliant_to_decreasing_rule("223450".to_string()));
+    }
+
+    #[test]
+    fn test_no_more_than_double() {
+
+//        assert_eq!(false, compliant_to_no_more_than_double("111111".to_string()), "not compliant to double rule 1");
+//        assert_eq!(true, compliant_to_no_more_than_double("113311".to_string()), "not compliant to double rule 1");
+        assert_eq!(compliant_to_no_more_than_double("11".to_string()), true, "not compliant to double rule 1");
+        assert_eq!(compliant_to_no_more_than_double("111".to_string()), false, "not compliant to double rule 1");
+//        assert_eq!(true, compliant_to_no_more_than_double("113334".to_string()), "not compliant to double rule 1");
+//        assert_eq!(true, compliant_to_no_more_than_double("112233".to_string()), "compliant to double rule");
+//        assert_eq!(true, compliant_to_no_more_than_double("111122".to_string()), "compliant to double rule");
+        assert_eq!(compliant_to_no_more_than_double("123444".to_string()), false, "not compliant to double rule 2");
     }
 }
--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -1,6 +1,8 @@
-use day04::part1;
+use day04::{part1, part2};
 
 fn main() {
     let result1 = part1();
-    println!("The number of password compliant to rules is {}", result1)
+    let result2 = part2();
+    println!("The number of password compliant to rules part 1 is {}", result1);
+    println!("The number of password compliant to rules part 2 is {}", result2);
 }
