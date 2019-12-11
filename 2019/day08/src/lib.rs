b3bdeb665212eb476faaf4d5c548aaf55b0d5a96
Yannick Guern <dev@guern.eu>
2019-12-11 03:02:43 +0100

    feat(day08): Part 2

--- 2019/day08/src/lib.rs
+++ 2019/day08/src/lib.rs
@@ -66,10 +66,74 @@ pub fn part_1() -> u32 {
     (*nb_2 * *nb_1) as u32
 }
 
+fn get_final_image(layers : Vec<Vec<char>>, image_size: u32) -> Vec<char>{
+
+    let mut output = vec![];
+    let mut current_layer = 0;
+    let nb_layer = layers.len();
+    for i in 0usize..image_size as usize {
+
+        let pixel = match layers[current_layer][i] {
+
+            x @ '1' | x @ '0' => x,
+            '2' => {
+                let mut current_pixel =' ';
+                loop {
+
+                    if current_layer > nb_layer {
+                        println!("overflow");
+                        current_layer = 0;
+                        break;
+                    }
+
+                    current_layer+=1;
+
+                    current_pixel = layers[current_layer][i];
+                    if current_pixel != '2' {
+                        // we come back to the top layer
+                        current_layer = 0;
+                        break;
+                    }
+                }
+                current_pixel
+            },
+            _ => panic!("Unknown character")
+        };
+        output.push(pixel)
+    }
+
+    output
+}
+
+pub fn part_2() {
+    let path = PathBuf::from("./assets/prod.txt");
+    let input = read_from_file(path).unwrap();
+    let layers = get_layers_from_string(input, 25*6);
+    let result = get_final_image(layers, 25*6);
+
+    println!();
+
+    for (i,c) in result.into_iter().enumerate() {
+
+
+        if i % 25 == 0 && i !=0 {
+            println!()
+        }
+
+        if c == '1' {
+            print!("{}", '#')
+        } else {
+            print!("{}", " ")
+        }
+    }
+
+    println!()
+}
+
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_layers_from_string, read_from_file, get_count_by_elements};
+    use crate::{get_layers_from_string, read_from_file, get_count_by_elements, get_final_image};
     use std::path::PathBuf;
 
     #[test]
@@ -101,5 +165,15 @@ mod tests {
         assert_eq!(nb_2, &2);
         assert_eq!(nb_1, &4);
         assert_eq!(nb_0, &3);
+    }
+
+
+    #[test]
+    fn test_get_final_image() {
+        let input = "0222112222120000".to_string();
+        let layers = get_layers_from_string(input, 2*2);
+        let expected = vec!['0','1','1','0'];
+        let result = get_final_image(layers, 2*2);
+        assert_eq!(result, expected);
     }
 }
--- 2019/day08/src/main.rs
+++ 2019/day08/src/main.rs
@@ -1,6 +1,7 @@
-use day08::part_1;
+use day08::{part_1, part_2};
 
 fn main() {
     let result1 = part_1();
-    println!("The result of part 1 is {}", result1)
+    part_2();
+    println!("The result of part 1 is {}", result1);
 }
